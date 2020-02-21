19fa4d4cea68d74e786c983ad5655e4bd7ce630b
lauripalonen <palonen.lauri@gmail.com>
2020-02-21 09:59:54 +0200

    remove unnecessary route

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,11 +2,6 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', async(request, response) => {
-  const users = await User.find({})
-  response.json(users.map(user => user.toJSON()))
-})
-
 usersRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
