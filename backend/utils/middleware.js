003b728ea7ab33045f3dbdb6bf7a4417152d99b9
lauripalonen <palonen.lauri@gmail.com>
2020-02-23 00:16:38 +0200

    fix typo

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,5 +1,5 @@
 const requestLogger= (request, response, next) => {
-  console.log('Method: ', request.methdod)
+  console.log('Method: ', request.method)
   console.log('Path: ', request.path)
   console.log('Body: ', request.body)
   console.log('--------------------')
